8fb61162c10ae6ef2994b04a9dbaa0ee8f13441d
DESKTOP-7K95UJ7\User <jeoroneithebest@hotmail.com>
2019-02-12 00:50:47 -0200

    Exemplos capitulo 2

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,49 @@
 package com.company;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.Consumer;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        Capitulo1();
+        Capitulo2();
+    }
+
+    // Capitulo 2
+    public static void Capitulo1(){
+        System.out.println("¿ Capitulo 1 ? \n\n");
+    }
+
+    // Capitulo 2
+    public static void Capitulo2(){
+        System.out.println("Capitulo 2 \n");
+
+        Usuario usuario1 = new Usuario("Jeoronei", 10);
+        Usuario usuario2 = new Usuario("Pedro", 5);
+        Usuario usuario3 = new Usuario("Thiago", 7);
+
+        List<Usuario> lista = Arrays.asList(usuario1, usuario2, usuario3);
+
+        // Antiga forma
+        for (Usuario usuario : lista) {
+            System.out.println("Exemplo 1: " + usuario.getNome());
+        }
+
+        System.out.println("");
+
+        // Com Consumer
+        lista.forEach(new Consumer<Usuario>() {
+            @Override
+            public void accept(Usuario usuario) {
+                System.out.println("Exemplo 2: " + usuario.getNome());
+            }
+        });
+
+        System.out.println("");
+
+        // Com Lambda
+        lista.forEach( u -> System.out.println("Exemplo 3: " + u.getNome()));
     }
 }
--- src/com/company/Usuario.java
+++ src/com/company/Usuario.java
@@ -1,4 +1,25 @@
 package com.company;
 
 public class Usuario {
+    private String nome;
+    private int pontos;
+    private boolean moderador;
+
+    public Usuario(String nome, int pontos) {
+        this.pontos = pontos;
+        this.nome = nome;
+        this.moderador = false;
+    }
+    public String getNome() {
+        return nome;
+    }
+    public int getPontos() {
+        return pontos;
+    }
+    public void tornaModerador() {
+        this.moderador = true;
+    }
+    public boolean isModerador() {
+        return moderador;
+    }
 }
